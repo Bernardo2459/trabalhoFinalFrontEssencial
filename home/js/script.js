265ff646b3de431edbf77aec7ed32a23756698b0
CSindorf <catarina.sindorf@gmail.com>
2022-09-28 21:13:39 -0300

    modal

--- home/js/script.js
+++ home/js/script.js
@@ -12,29 +12,44 @@ async function produtos() {
           <div class="card-body">
             <h4>${item.Tenis}</h4>
             <p class="produto-descricao">${item.mini}</p>
-            <p class="produto-preco">${item.preco}</p>
-            <p class="produto-info"><a data-toggle="modal" data-target="#myModal">Mais informações</a></p>
+            <p class="produto-preco"><span>R$ </span>${item.preco}</p>
+            <p class="produto-info">
+            <a data-toggle="modal" data-target="#myModal" onclick="produtosModal(${item.id})">Mais informações</a>
+            </p>
           </div>
         </div>
       </div>
     `
 
     document.getElementById("produtos").insertAdjacentHTML('beforeend', card)
-    })
 
-    /*produtos.map(item => {
-        var card = `
+    //produtosModal(item.id)
+    })
+    
+
+}
+
+async function produtosModal(produto) {
+  var respostaAPI = await fetch(`https://mocki.io/v1/f3db575d-0282-4b74-a286-d33545405316`)
+
+  var produtos = await respostaAPI.json();
+  var produto = await produtos.filter(prod => prod.id == produto)
+
+  console.log(produto)
+
+  produto.map(item => {
+      var card = `
+      <div class="modal-body">
       <div class="imagem-modal">
-            <img src="${item.imagem} alt="Produto 1">
-          </div>
-          <h4>${item.nome}</h4>
-          <p class="texto-modal">
-          ${item.nome} 
-          <p class="produto-preco">            <p class="produto-preco">${item.preco}</p>
-          </p>
-          </p><button id="btn-entrar" type="button" class="btn btn-primary">Ver produto</button>
-    `
-    document.getElementById("modal").insertAdjacentHTML('beforeend', card)
-    })*/
+        <img src=${item.imagem} alt="Produto 1">
+      </div>
+      <h4>${item.nome}</h4>
+      <p class="texto-modal">${item.descricao}</p>
+      <p class="produto-preco"><span>R$ </span>${item.preco}</p>
+      <button id="btn-entrar" type="button" class="btn btn-primary">Ver produto</button>
+      </div>
+  `
+  document.getElementById("modal").insertAdjacentHTML('beforeend', card)
+  })
 
 }
